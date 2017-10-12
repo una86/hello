fb1d8800f5adbd9e91d5d357b23a02b0c11400be
una86 <unaben@gmail.com>
2017-10-12 22:02:41 +0100

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void doSomething()
+    {
+
+    }
+    public static void main(String[] args) {
+        doSomething ();
+        System.out.println("Hello World!");
+    }
+}
