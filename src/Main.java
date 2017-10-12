cfbcbbf6790b044233ac09b833bcd99aa566f908
una86 <unaben@gmail.com>
2017-10-12 22:14:33 +0100

    made schanges

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void doSomething()
     {
-
+        System.out.println("Hello Una!");
     }
     public static void main(String[] args) {
         doSomething ();
